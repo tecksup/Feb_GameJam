20260186c2ad5fad684e36f89bfa6a019f3d78b7
tecksup <eklofandy@gmail.com>
2017-10-03 21:30:41 -0700

    added map manuvering to test scene

--- core/src/com/thecubecast/ReEngine/GameStates/TestState.java
+++ core/src/com/thecubecast/ReEngine/GameStates/TestState.java
@@ -12,22 +12,28 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.thecubecast.ReEngine.Data.Common;
 import com.thecubecast.ReEngine.Data.GameStateManager;
 
 public class TestState extends GameState {
 	
+	float w;
+    float h;
+    
     Texture img;
     TiledMap tiledMap;
     OrthographicCamera camera;
     TiledMapRenderer tiledMapRenderer;
+    
+    float camZoom = 1.0f;
 	
 	public TestState(GameStateManager gsm) {
 		super(gsm);
 	}
 	
 	public void init() {
-        float w = Gdx.graphics.getWidth();
-        float h = Gdx.graphics.getHeight();
+        w = Gdx.graphics.getWidth();
+        h = Gdx.graphics.getHeight();
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false,w,h);
@@ -45,44 +51,69 @@ public class TestState extends GameState {
 		 Gdx.gl.glClearColor(1, 0, 0, 1);
 	        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 	        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	        gsm.Render.GUIDeco(g, 0, 0, "TEST");
 	        camera.update();
 	        tiledMapRenderer.setView(camera);
 	        tiledMapRenderer.render();
+	        
+	        //gsm.Render.GUIDeco(g, 0, 0, "TEST");
 	}
 	
 	public void handleInput() {
 
 		if(Gdx.input.isTouched()) {
-			camera.translate(-Gdx.input.getDeltaX(), Gdx.input.getDeltaY());
+			camera.translate(-Gdx.input.getDeltaX()*camZoom, Gdx.input.getDeltaY()*camZoom);
+			camera.update();
+			
+			if (Gdx.input.isKeyPressed(Keys.CONTROL_LEFT)) {
+				if (Gdx.input.getDeltaY() < 0) { // ZOOMS OUT
+					if ((camZoom + 0.1f) < 5)
+					camZoom = camZoom + 0.1f;
+					camera.setToOrtho(false, w*camZoom, h*camZoom);
+					camera.update();
+				}
+				if (Gdx.input.getDeltaY() > 0) {  // ZOOMS IN
+					if ((camZoom - 0.1f) > 0.1)
+						camZoom = camZoom - 0.1f;
+					camera.setToOrtho(false, w*camZoom, h*camZoom);
+					camera.update();
+				}
+			}
+		}
+		
+		if (Gdx.input.isKeyPressed(Keys.CONTROL_LEFT) && Gdx.input.isKeyPressed(Keys.NUM_0)) {  // RESETS THE ZOOM
+			camZoom = 1;
+			camera.setToOrtho(false, w*camZoom, h*camZoom);
 			camera.update();
 		}
 		
 		if (Gdx.input.isKeyJustPressed(Keys.ENTER)) { //KeyHit
-			tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
+			tiledMap.getLayers().get(1).setVisible(!tiledMap.getLayers().get(1).isVisible());
 			
 		}
+		
 		if (Gdx.input.isKeyPressed(Keys.UP)) { //KeyHit
 			if (Gdx.input.isKeyPressed(Keys.CONTROL_LEFT) || Gdx.input.isKeyPressed(Keys.CONTROL_RIGHT))
-				camera.translate(0,1);
+				camera.translate(0,4);
 			else 
 				camera.translate(0,16);
 		}
 		if (Gdx.input.isKeyPressed(Keys.DOWN)) { //KeyHit
 			if (Gdx.input.isKeyPressed(Keys.CONTROL_LEFT) || Gdx.input.isKeyPressed(Keys.CONTROL_RIGHT))
-				camera.translate(0,-1);
+				camera.translate(0,-4);
 			else 
 				camera.translate(0,-16);
 		}
 		if (Gdx.input.isKeyPressed(Keys.LEFT)) { //KeyHit
 			if (Gdx.input.isKeyPressed(Keys.CONTROL_LEFT) || Gdx.input.isKeyPressed(Keys.CONTROL_RIGHT))
-				camera.translate(-1,0);
+				camera.translate(-4,0);
 			else 
 				camera.translate(-16,0);
 			
 		}
 		if (Gdx.input.isKeyPressed(Keys.RIGHT)) { //KeyHit
 			if (Gdx.input.isKeyPressed(Keys.CONTROL_LEFT) || Gdx.input.isKeyPressed(Keys.CONTROL_RIGHT))
-				camera.translate(1,0);
+				camera.translate(4,0);
 			else 
 				camera.translate(16,0);
 			
